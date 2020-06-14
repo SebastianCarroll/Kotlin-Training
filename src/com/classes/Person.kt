71cc5f0c70ac6ef88105dbe682a4676f1f8c6ca7
Sebastian Carroll <seabass25@hotmail.com>
2020-06-14 13:15:37 +0100

    Second pass at classes - open, abstract, override

--- src/com/classes/Person.kt
+++ src/com/classes/Person.kt
@@ -2,21 +2,37 @@ package com.classes
 
 
 fun main(args: Array<String>) {
-    val p = Person()
-    println(p)
+    val s = Student()
+    println(s)
 }
 
-class Person {
+abstract class Person {
     /* public, final by default (cannot be inherited if explicitly allowed to)
-// use open to allow inheritance
-// support abstract classes
-// Sealed class
-// Class constructors
-// data classes
-*/
-    var firstName:String =""
-    var lastName:String =""
+    * use open to allow inheritance
+    * support abstract classes
+    * -- abstract classes and functions
+    * -- abstract fns must be implemented
+    * -- open fns may be overridden
+    * -- everything else cannot be overriden
 
-    open fun getName() :String ="$firstName $lastName"
+    * Does not use packages for visibility scoping, just a way of collecting things together
+    * Internal keyword - makes things visible to everything in that module, could be
+    * a compilation module.
+    *
+    * sealed classes - 'enums on steroids', algebraic data types,
+    * - used to restrict hierarchies
+    * - sealed keyword
+    * - define which classes can derive from class Event
+    */
+    var firstName: String = ""
+    var lastName: String = ""
+
+    open fun getName(): String = "$firstName $lastName" // *can* be overriden
+    abstract fun getAddress(): String // must be overridden/implemeted
 }
 
+class Student : Person() {
+    override fun getAddress(): String {
+        return ""
+    }
+}
