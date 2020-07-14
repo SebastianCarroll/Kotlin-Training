a8c5a0fb370fdc70e4aa561de682b240ba17a424
Sebastian Carroll <seabass25@hotmail.com>
2020-07-14 21:40:09 +0100

    Java functional interop notes

---
+++ src/com/functions/javaInterfacesInterOp.kt
@@ -0,0 +1,8 @@
+package com.functions
+
+/*
+Can convert from Kotlin lambas to java SAM interfaces
+Not sure what this implies really. Can call into the java code from Kotlin?
+
+Might need a SAM constructor at some point if the compiler can't work out the types expected
+ */
