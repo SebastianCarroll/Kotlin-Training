89619e6e6988ea58332331ddfab1cbf2910b466e
Sebastian Carroll <seabass25@hotmail.com>
2020-07-14 21:34:21 +0100

    Notes on sequences

---
+++ src/com/functions/infiniteData.kt
@@ -0,0 +1,13 @@
+package com.functions
+
+/*
+Use .asSequence to convert a list to a sequence
+
+Sequences have transformation and terminal operations
+Nothing will happen if no terminal operations are called
+
+Lazy evaluated
+
+Can pass around a partially evaluated stream without operating on it until we operate with the
+terminal op
+ */
